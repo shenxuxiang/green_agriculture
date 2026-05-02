8104981b73cc2521c28140e67624c45d6d298b17
shenxuxiang <shen_xuxiang@163.com>
2026-05-02 20:32:36 +0800

    feat: 添加lint代码检查

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.android.build.gradle.BaseExtension
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -7,4 +9,19 @@ plugins {
     id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
     id("com.google.dagger.hilt.android") version "2.57.1" apply false
     id("androidx.navigation.safeargs") version "2.9.7" apply false
+}
+
+subprojects {
+    pluginManager.withPlugin("com.android.base") {
+        extensions.configure<BaseExtension> {
+            lintOptions.apply {
+                lintConfig = rootProject.file("lint.xml")
+                htmlOutput = rootProject.file("build/reports/lint-${project.name}.html")
+                xmlOutput = rootProject.file("build/reports/lint-${project.name}.xml")
+                isCheckDependencies = false
+                isCheckReleaseBuilds = true
+                isAbortOnError = false
+            }
+        }
+    }
 }
